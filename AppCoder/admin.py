913c46571b0926b24739c75847ee4285928d7097
rodrigobravo <bravorodrigo1986@gmail.com>
2024-03-22 14:40:57 -0300

    Proyecto Final version 1.0

--- AppCoder/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from AppCoder.models import *
-
-# Register your models here.
-
-admin.site.register(Equipo)
-admin.site.register(Jugador)
-admin.site.register(Tecnico)
